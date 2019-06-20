b3c630beccf53c95769ba6380b92049166a8fa6f
amirhaf76 <amir.haf76@gmail.com>
2019-06-20 15:01:53 +0430

    clean code

--- src/Test/MusicTest.java
+++ src/Test/MusicTest.java
@@ -6,35 +6,27 @@ import Model.Music;
 import mp3agic.InvalidDataException;
 import mp3agic.UnsupportedTagException;
 import org.junit.jupiter.api.*;
-
-import javax.imageio.ImageIO;
 import java.io.*;
 import java.sql.Time;
-import java.util.Scanner;
+
 
 class MusicTest {
 
     private static Music music;
-    private static File file;
-    private static Time time;
-    private static RandomAccessFile imageFile;
-
-
 
     @BeforeAll
     static void setUp() {
+
         String parent = "I:\\Amir.haf76's Files\\Univercity\\" +
                 "ProjectOfJava\\src\\Test\\FileOfTest\\";
 
-        file =  new File(parent + "Sham Pain - Five Finger Death Punch.mp3");
-        time = new Time(System.currentTimeMillis());
+        File file = new File(parent + "Sham Pain - Five Finger Death Punch.mp3");
+        Time time = new Time(System.currentTimeMillis());
 
 
         try {
 
-            imageFile = new RandomAccessFile(parent + "Sham Pain.jpg","r");
-
-            music = new Music(file,time);
+            music = new Music(file, time);
         } catch (UnsupportedTagException | IOException | InvalidDataException e) {
             e.printStackTrace();
         }

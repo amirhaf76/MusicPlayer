ad34a6ea0011bbf1e06e065d53bd91b0cb8aa9ea
amirhaf76 <amir.haf76@gmail.com>
2019-06-21 13:25:01 +0430

    add some part

--- src/Model/MusicPlayer.java
+++ src/Model/MusicPlayer.java
@@ -1,10 +1,16 @@
 package Model;
 
+import javazoom.jl.decoder.JavaLayerException;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MusicPlayer {
 
     private ArrayList<Music> musics = new ArrayList<>();
+
 
     public void addMusic(Media media) {
 
@@ -24,7 +30,31 @@ public class MusicPlayer {
 
     }
 
-    public  void playMusic() {
+    public static void playMusic(File file) throws JavaLayerException, IOException, InterruptedException, UnsupportedAudioFileException {
 
+        System.out.println("here");
+        MusicController p = new MusicController(file);
+        p.startMusic(95000);
+        System.out.println("here2");
+
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("here3");
+
+//        System.out.println(p.getPosition());
+
+        System.out.println("here4");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("here5");
+//        System.out.println(p.getPosition());
+        System.out.println("here6");
     }
 }

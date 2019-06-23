b7f23c02355b1a173288bbdfaea3130ceac22718
amirhaf76 <amir.haf76@gmail.com>
2019-06-23 14:30:12 +0430

    add skip Control

--- src/Model/CONTROL.java
+++ src/Model/CONTROL.java
@@ -3,8 +3,10 @@ package Model;
 enum CONTROL {
     PAUSE(0, "Pause"), PLAYING(1, "Playing"),STOP(2, "Stop"),
     NEXT(3, "Next"), PREVIOUS(4, "Previous"),
-    NOTSTARTED(5, "NotStart"),FIINISH(6, "Finish"),
-    ALWAYS(7, "Always"), ONECE( 8, "Once"), JUSTTHIS(9, "JustThis");
+    NOTSTARTED(5, "NotStart"), FINISH(6, "Finish"),
+    ALWAYS(7, "Always"), ONECE( 8, "Once"), JUSTTHIS(9, "JustThis"),
+    SKIP(10, "Skip");
+
 
     private int state;
     private String name;

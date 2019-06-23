865eb4f4480fe41530c16e0b4624f27542d84740
amirhaf76 <amir.haf76@gmail.com>
2019-06-24 00:43:52 +0430

    cleaning

--- src/Model/Control.java
+++ src/Model/Control.java
@@ -1,17 +1,16 @@
 package Model;
 
-enum CONTROL {
+enum Control {
     PAUSE(0, "Pause"), PLAYING(1, "Playing"),STOP(2, "Stop"),
     NEXT(3, "Next"), PREVIOUS(4, "Previous"),
     NOTSTARTED(5, "NotStart"), FINISH(6, "Finish"),
-    ALWAYS(7, "Always"), ONECE( 8, "Once"), JUSTTHIS(9, "JustThis"),
-    SKIP(10, "Skip");
+    SKIP(7, "Skip");
 
 
     private int state;
     private String name;
 
-    CONTROL(int state, String name) {
+    Control(int state, String name) {
         this.state = state;
         this.name = name;
     }

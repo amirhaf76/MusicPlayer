7571c29c1dd62a37a79161622eeb2223ba635f8f
amirhaf76 <amir.haf76@gmail.com>
2019-06-25 17:00:38 +0430

    create clientHandler

--- src/network/ClientHandler.java
+++ src/network/ClientHandler.java
@@ -1,27 +1,60 @@
 package network;
 
+import Model.Command;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class ClientHandler implements Runnable {
 
     private final DataInputStream dis;
     private final DataOutputStream dom;
+    private final Socket client;
+    private final Object lock = new Object();
+
+    private ArrayList<Command> commands = new ArrayList<>();
 
 
     public ClientHandler(Socket client) throws IOException {
+        this.client = client;
         this.dis = new DataInputStream(client.getInputStream());
         this.dom = new DataOutputStream(client.getOutputStream());
     }
 
-    public void closeHandler() {
-        // TODO: 6/25/2019  close handler
+    public void closeHandler() throws IOException {
+        synchronized (lock) {
+            dis.close();
+            dom.close();
+            client.close();
+        }
     }
 
     @Override
     public void run() {
 
     }
+
+    public SocketAddress getAddressOfEndPoint() {
+        return client.getLocalSocketAddress();
+    }
+
+    public void addCommand(Command command) {
+        if ( !commands.contains(command) ) {
+           commands.add(command);
+        }
+    }
+
+    public ArrayList<Command> sendCommands() {
+        ArrayList<Command> commands = new ArrayList<>(this.commands);
+        this.commands.removeAll(commands);
+        return commands;
+    }
+
+
+
 }

8afc001fed6f4bc3de6f2cb9475763ad4c1bf69a
amirhaf76 <amir.haf76@gmail.com>
2019-06-26 08:42:08 +0430

    add method for getting and setting library for user

--- src/Model/User.java
+++ src/Model/User.java
@@ -6,11 +6,14 @@ public class User implements Serializable {
 
     private String name;
     private String password;
-    private Library library;
+    private transient Library library;
 
     private static final long serialVersionUID = 1398441L;
 
-
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
 
     public String getName() {
         return name;
@@ -18,5 +21,13 @@ public class User implements Serializable {
 
     public String getPassword() {
         return password;
+    }
+
+    public Library getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
     }
 }

b31ab5746aaa479900efa7e41e58d763faf7548c
amirhaf76 <amir.haf76@gmail.com>
2019-06-26 08:41:20 +0430

    create class for saving and reloading User and his/her library

---
+++ src/storage/ReloadFile.java
@@ -0,0 +1,35 @@
+package storage;
+
+import Model.Library;
+import Model.User;
+
+import java.io.*;
+
+public class ReloadFile {
+
+    private final String parent;
+    private final User user;
+
+    public ReloadFile(User user) {
+        this.user = user;
+        parent = "User: " + user.getName();
+    }
+
+    public User reloadUser() throws IOException, ClassNotFoundException {
+        FileInputStream file = new FileInputStream(
+                new File(parent + "\\" + user.getName() + ".ur")
+        );
+        ObjectInputStream in = new ObjectInputStream(file);
+
+        return (User) in.readObject();
+    }
+
+    public Library reloadLibrary() throws IOException, ClassNotFoundException {
+        FileInputStream file = new FileInputStream(
+                new File(parent + "\\" + user.getName() + ".lb")
+        );
+        ObjectInputStream in = new ObjectInputStream(file);
+
+        return (Library) in.readObject();
+    }
+}
---
+++ src/storage/Storage.java
@@ -0,0 +1,53 @@
+package storage;
+
+import Model.User;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
+public class Storage {
+
+    private final String parent;
+    private final User user;
+
+    public Storage(User user) {
+        this.user = user;
+        parent = "User: " + user.getName();
+    }
+
+    public void makeFile() {
+        File file = new File( parent );
+        if ( !file.isDirectory() ) {
+            if ( !file.mkdir() ) {
+                JOptionPane.showMessageDialog( new Frame(),
+                        "There is problem in making folder",
+                        "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public void saveUser() throws IOException {
+        FileOutputStream savedFile = new FileOutputStream(
+                new File(parent + "\\" + user.getName() + ".ur")
+        );
+        ObjectOutputStream out = new ObjectOutputStream(savedFile);
+        out.writeObject(user);
+
+        out.close();
+        savedFile.close();
+
+    }
+
+    public void saveLibrary() throws IOException {
+        FileOutputStream savedFile = new FileOutputStream(
+                new File(parent + "\\" + user.getName() + ".b" )
+        );
+        ObjectOutputStream out = new ObjectOutputStream(savedFile);
+        out.writeObject(user.getLibrary());
+
+        out.close();
+        savedFile.close();
+    }
+
+}

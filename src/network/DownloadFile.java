744087fcf2b9fd67277e16be4cf161b9331308e2
amirhaf76 <amir.haf76@gmail.com>
2019-06-27 13:25:13 +0430

    complete DownloadFile class

--- src/network/DownloadFile.java
+++ src/network/DownloadFile.java
@@ -1,19 +1,70 @@
 package network;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
 
 public class DownloadFile {
-    private final File file;
-    private long part;
-    private final int mgByte = 1024 * 1024;
 
-    public DownloadFile(File file) {
+    private final File file;
+    private final ArrayList<byte[]> data = new ArrayList<>();
+    private int partNumber = 0;
+    private boolean end = false;
+    private boolean ready = false;
+    private final int MB = 1024 * 1024;
+
+    public DownloadFile(File file) throws FileNotFoundException {
         this.file = file;
     }
 
-    public void takeAPart() throws FileNotFoundException {
-        Scanner in = new Scanner(file);
+    public void prepareForSending() throws IOException {
+        BufferedInputStream buffer = new BufferedInputStream(new FileInputStream(file));
+        byte[] temp = new byte[MB];
+        int bytesAccount;
+
+        while ( (bytesAccount = buffer.read(temp)) != -1 ) {
+            if (bytesAccount == MB) {
+                data.add(temp);
+            }
+            else {
+                byte[] bytes = new byte[bytesAccount];
+                System.arraycopy(temp, 0, bytes, 0, bytesAccount);
+                data.add(bytes);
+            }
+        }
+
+        ready = true;
+        buffer.close();
+    }
+
+    public ArrayList<byte[]> getData() {
+        return data;
+    }
+
+    public byte[] getPartOfData() {
+        byte[] temp = new byte[0];
+
+        if ( partNumber >= 0 && partNumber < data.size() ) {
+
+            if ( partNumber == data.size()-1 ) { // last part : data.size() - 1
+                end = true;
+            }
+
+            temp = data.get(partNumber);
+            partNumber++;
+        }
+
+        return temp;
+    }
+
+    public boolean isEnd() {
+        return end;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    public int getPartNumber() {
+        return partNumber;
     }
 }

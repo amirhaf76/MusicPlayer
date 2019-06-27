a8693030e905a453742a0f35bf23c842f4540caf
amirhaf76 <amir.haf76@gmail.com>
2019-06-27 13:22:41 +0430

    add some method add some part that each Package include them and it's not possible change it.

--- src/network/Package.java
+++ src/network/Package.java
@@ -2,17 +2,47 @@ package network;
 
 import Model.Music;
 import Model.enumeration.Command;
-
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Package {
+public class Package implements Serializable {
     private final ArrayList<Command> commands;
-    private final ArrayList<Music> SharedMusic;
-    private DownloadFile downloadFile;
+    private final ArrayList<Music> requestedSharedMusic;
+    private final Music getMusic;
+    private final byte[] data;
+    private final boolean endDownload;
 
-    public Package(ArrayList<Command> commands, ArrayList<Music> sharedMusic) {
+    private static final long serialVersionUID = 1396461L;
+
+
+    public Package(ArrayList<Command> commands, ArrayList<Music> requestedSharedMusic,
+                   Music getMusic, byte[] data, boolean endDownload) { // date : data of getMusic
         this.commands = commands;
-        SharedMusic = sharedMusic;
+        this.requestedSharedMusic = requestedSharedMusic;
+        this.getMusic = getMusic;
+        this.data = data;
+        this.endDownload = endDownload;
     }
     // TODO: 6/26/2019 sharedList
+
+
+    public ArrayList<Command> getCommands() {
+        return commands;
+    }
+
+    public ArrayList<Music> getRequestedSharedMusic() {
+        return requestedSharedMusic;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public Music getGetMusic() {
+        return getMusic;
+    }
+
+    public boolean isEndDownload() {
+        return endDownload;
+    }
 }

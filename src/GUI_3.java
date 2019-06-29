177504e7641296c0191d5849f68f6f102c2b0d25
amirhaf76 <amir.haf76@gmail.com>
2019-06-29 10:20:24 +0430

    edit gui

---
+++ src/GUI_3.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+
+@SuppressWarnings("Duplicates")
+public class GUI_3 extends javax.swing.JFrame {
+
+
+    public GUI_3() {
+        initComponents();
+    }
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jFileChooser1 = new javax.swing.JFileChooser();
+
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        ImageIcon img = new ImageIcon("find-on-page.png");
+        setIconImage(img.getImage());
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jFileChooser1, javax.swing.GroupLayout.DEFAULT_SIZE, 902, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jFileChooser1, javax.swing.GroupLayout.DEFAULT_SIZE, 475, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        setResizable(false);
+        setLocation(400,300);
+        setVisible(true);
+        pack();
+    }
+    private javax.swing.JFileChooser jFileChooser1;
+    private javax.swing.JPanel jPanel1;
+}

e5d06717c38343802328281aab9919afc7ab82aa
amirhaf76 <amir.haf76@gmail.com>
2019-07-04 19:01:05 +0430

    the Music class is cleaned

--- src/Model/Music.java
+++ src/Model/Music.java
@@ -2,17 +2,16 @@ package Model;
 
 import mp3agic.*;
 import java.io.*;
-import java.sql.Time;
-import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Formatter;
 import java.util.Objects;
-import java.util.SimpleTimeZone;
-import java.util.logging.SimpleFormatter;
+import javax.swing.*;
+import static javax.swing.JOptionPane.*;
 
 
-public class Music extends Media implements Serializable {
 
-    private boolean loaded = false;
+public class Music extends Media {
+
     private String title = "<nothing>";
     private Artist artist = new Artist("<nothing>");
     private Album album = new Album("<nothing>",artist);
@@ -24,30 +23,26 @@ public class Music extends Media implements Serializable {
     private int frames = 0;
     private long time = 0;
 
+    private boolean favorite = false;
+    private boolean shared = false;
 
-    public Music(File mediaFile, Time time) throws InvalidDataException, IOException, UnsupportedTagException {
+
+    public Music(File mediaFile, LocalDateTime time) {
         super(mediaFile, time);
         loadMusic();
     }
 
-    private void loadMusic() throws InvalidDataException, IOException, UnsupportedTagException {
+    private void loadMusic() {
         this.getID3v1();
         this.getID3v2();
         imageAlbum = getArtWork();
-        loaded = true;
-    }
-
-    public boolean isLoaded() {
-        return loaded;
     }
 
     public byte[] getImageAlbum() {
         return imageAlbum;
     }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
 
     public Artist getArtist() {
         return artist;
@@ -81,71 +76,117 @@ public class Music extends Media implements Serializable {
         return time;
     }
 
+    public boolean isFavorite() {
+        return favorite;
+    }
 
+    public boolean isShared() {
+        return shared;
+    }
 
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
 
+    public void setShared(boolean shared) {
+        this.shared = shared;
+    }
 
-    private void getID3v1_2() throws IOException {
-
-        byte[] buffer = new byte[125];
-        RandomAccessFile file = new RandomAccessFile(super.getMediaFile(), "r");
-        file.seek(file.length() - 125);
-
-        if (file.read(buffer,0,125) != 125 ) {
-            file.close();
-            throw new IOException();
+    private void getID3v1() {
+        ID3v1 id3v1 = null;
+        try {
+            id3v1 = (new Mp3File(getFile().getPath())).getId3v1Tag();
+            title = id3v1.getTitle();
+            artist = Artist.isThisArtist(id3v1.getArtist());
+            album = new Album(id3v1.getAlbum(), artist);
+            comment = id3v1.getComment();
+            year = id3v1.getYear();
+            track = id3v1.getTrack();
+            genre = id3v1.getGenre();
+        } catch (IOException e) {
+            showMessageDialog(new JFrame(), "There is error in opening file",
+                    "Error", ERROR_MESSAGE);
+        } catch (UnsupportedTagException e) {
+            showMessageDialog(new JFrame(), "This tag is not supported",
+                    "Error", ERROR_MESSAGE);
+        } catch (InvalidDataException e) {
+            showMessageDialog(new JFrame(), "The format is not valid",
+                    "Error", ERROR_MESSAGE);
         }
 
-        String details = new String(buffer,0,125);
-        title = details.substring(0, 30).trim();
-        artist = new Artist(details.substring(30, 60).trim());
-        album = new Album(details.substring(60, 90).trim(), artist);
-        year = details.substring(90, 94).trim();
-        comment = details.substring(94,122).trim();
-        if ( comment.length() == 30 || comment.length() == 29) {
-            comment = details.substring(94, 124);
-        } else {
-            if ( buffer[122] == 0 ){
 
-                track = "" + (int) buffer[123];
-            }
+    }
+
+    private void getID3v2() {
+        Mp3File mp3File = null;
+        try {
+            mp3File = new Mp3File(getFile().getPath());
+            time = mp3File.getLengthInSeconds();
+            frames = mp3File.getFrameCount();
+        } catch (IOException e) {
+            showMessageDialog(new JFrame(), "There is error in opening file",
+                    "Error", ERROR_MESSAGE);
+        } catch (UnsupportedTagException e) {
+            showMessageDialog(new JFrame(), "This tag is not supported",
+                    "Error", ERROR_MESSAGE);
+        } catch (InvalidDataException e) {
+            showMessageDialog(new JFrame(), "The format is not valid",
+                    "Error", ERROR_MESSAGE);
         }
-        genre = (int) details.charAt(124);
 
     }
 
-    private void getID3v1() throws InvalidDataException, IOException, UnsupportedTagException {
-        ID3v1 id3v1 = (new Mp3File(getMediaFile().getPath())).getId3v1Tag();
-        title = id3v1.getTitle();
-        artist = new Artist(id3v1.getArtist());
-        album = new Album(id3v1.getAlbum(), artist);
-        comment = id3v1.getComment();
-        year = id3v1.getYear();
-        track = id3v1.getTrack();
-        genre = id3v1.getGenre();
+    private byte[] getArtWork() {
 
+        Mp3File mp3File = null;
+        try {
+            mp3File = new Mp3File(getFile().getPath());
+            if ( mp3File.hasId3v2Tag() ) {
 
-    }
+                byte[] imageBuffer = mp3File.getId3v2Tag().getAlbumImage();
 
-    private void getID3v2() throws InvalidDataException, IOException, UnsupportedTagException {
-        Mp3File mp3File = new Mp3File(getMediaFile().getPath());
-        time = mp3File.getLengthInSeconds();
-        frames = mp3File.getFrameCount();
-    }
-
-    private byte[] getArtWork() throws InvalidDataException, IOException, UnsupportedTagException {
-
-        Mp3File mp3File = new Mp3File(getMediaFile().getPath());
-        if ( mp3File.hasId3v2Tag() ) {
-
-            byte[] imageBuffer = mp3File.getId3v2Tag().getAlbumImage();
-
-            if ( imageBuffer != null ) {
-                return imageBuffer;
+                if ( imageBuffer != null ) {
+                    return imageBuffer;
+                }
             }
+        } catch (IOException e) {
+            showMessageDialog(new JFrame(), "There is error in opening file",
+                    "Error", ERROR_MESSAGE);
+        } catch (UnsupportedTagException e) {
+            showMessageDialog(new JFrame(), "This tag is not supported",
+                    "Error", ERROR_MESSAGE);
+        } catch (InvalidDataException e) {
+            showMessageDialog(new JFrame(), "The format is not valid",
+                    "Error", ERROR_MESSAGE);
         }
 
         return new byte[0];
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, artist, album);
+    }
+
+    @Override
+    public String toString() {
+        Formatter formatter = new Formatter();
+        int second = (int) time % 60;
+        int minute = (int) (( time - second) / 60) % 60;
+        int hour = (int) (time - second - minute * 60) /3600;
+        if ( hour == 0 ) {
+            formatter.format("%02d : %02d", minute, second);
+        } else {
+            formatter.format("%02d : %02d : %20d", hour, minute, second);
+        }
+        return "Title : " + title + '\n' +
+                artist + '\n' +
+                album + '\n' +
+                "Year : " + year + '\n' +
+                "Comment :" + comment + '\n' +
+                "Track : " + track + '\n' +
+                "Time : " + formatter;
     }
 
     @Override
@@ -158,28 +199,33 @@ public class Music extends Media implements Serializable {
                 album.equals(music.album);
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, artist, album);
-    }
 
-    @Override
-    public String toString() {
-        Formatter formatter = new Formatter();
-        int second = (int) time % 60;
-        int minute = (int) ( time - second) / 60;
-        int hour = (int) (time - second - minute * 60) /3600;
-        if ( hour == 0 ) {
-            formatter.format("%2d : %2d", minute, second);
-        } else {
-            formatter.format("%2d : %2d : %2d", hour, minute, second);
-        }
-        return "Title : " + title + '\n' +
-                artist + '\n' +
-                album + '\n' +
-                "Year : " + year + '\n' +
-                "Comment :" + comment + '\n' +
-                "Track : " + track + '\n' +
-                "Time : " + formatter;
-    }
+//        private void getID3v1_2() throws IOException {
+//
+//        byte[] buffer = new byte[125];
+//        RandomAccessFile file = new RandomAccessFile(super.getFile(), "r");
+//        file.seek(file.length() - 125);
+//
+//        if (file.read(buffer,0,125) != 125 ) {
+//            file.close();
+//            throw new IOException();
+//        }
+//
+//        String details = new String(buffer,0,125);
+//        title = details.substring(0, 30).trim();
+//        artist = new Artist(details.substring(30, 60).trim());
+//        album = new Album(details.substring(60, 90).trim(), artist);
+//        year = details.substring(90, 94).trim();
+//        comment = details.substring(94,122).trim();
+//        if ( comment.length() == 30 || comment.length() == 29) {
+//            comment = details.substring(94, 124);
+//        } else {
+//            if ( buffer[122] == 0 ){
+//
+//                track = "" + (int) buffer[123];
+//            }
+//        }
+//        genre = (int) details.charAt(124);
+//
+//    }
 }
